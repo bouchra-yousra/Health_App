a3c152327f409a3cf6dbd2547775c8c5ccf053ad
kacemws <berras.belkacem@gmail.com>
2020-07-12 14:36:39 +0100

    added google-services.json since it was missing, added the logged in user verification at the start of the main activity.

--- app/src/main/java/com/example/myhealthapp/MainActivity.java
+++ app/src/main/java/com/example/myhealthapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.myhealthapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+        String aux = currentUser == null? "no user" : "found user";
+        TextView tv1 = (TextView)findViewById(R.id.mainText);
+        tv1.setText(aux);
+        Log.d("TAG", "onStart: "+aux);
     }
 }
